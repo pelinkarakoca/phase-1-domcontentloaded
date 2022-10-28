5bca5ccd7e6ad24d118be4e5ac516a530c9f09b0
Pelin Karakoca <karakocapelin@gmail.com>
2022-10-28 15:35:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+const paragraph = document.getElementById("text");
+
+document.addEventListener("DOMContentLoaded", function(){
+    paragraph.textContent = "This is really cool!";
+})
+
